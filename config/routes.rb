f7c60cd1da82f473c5e20dc34ab54ccd55ffbac4
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-08-18 21:53:52 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root "messages#index"
-  resources :messages, only: [:index, :create]
+  resources :users, only: [:edit, :update]
 end
