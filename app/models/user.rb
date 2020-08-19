1b666d68f0b89802eece02cfb798033d6ea4c993
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-08-19 14:27:05 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
-
+  has_many :rooms, through: :room_users
+  has_many :room_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/migrate/20200819031816_create_rooms.rb
+++ db/migrate/20200819031816_create_rooms.rb
@@ -1,4 +1,5 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
+  assosiation
   def change
     create_table :rooms do |t|
       t.string :name, null:false
